c68a5f786e00fb26ae088bc61f6d2bb02443d613
Aborren <elias.renman00@gmail.com>
2019-09-17 13:12:35 +0200

    Created TeamsRoutes routes.
    Added routes
    Wrote comments for the different methods.

--- Routes/TeamRoutes.py
+++ Routes/TeamRoutes.py
@@ -1,3 +1,44 @@
 from flask import Blueprint, abort
 
 tr = Blueprint('tr', __name__)
+
+
+@tr.route('/create_team/', methods=['POST'])
+def create_team():
+    """
+    Creates a new team with the name of name specified in POST payload.
+    Returns a response as a str message, redirects response from error if it fails.
+    :rtype: str
+    """
+    pass
+
+
+@tr.route('/delete_team/', methods=['POST'])
+def delete_team():
+    """
+    Deletes a team from the database depending on the input written in the POST payload.
+    Returns a str saying Successfully deleted :placeholder_name: or error if error occurred or team did not exist.
+    :rtype: str
+    """
+    pass
+
+
+@tr.route('/update_team/', methods=['POST'])
+def update_team():
+    """
+    Updates a specific team name with a new name specified by the POST payload.
+    Returns a str saying Successfully updated team with a new name of :placeholder_name: or -
+    error if error occurred or team did not exist.
+    :rtype: str
+    """
+    pass
+
+
+@tr.route('/display_team/', methods=['POST'])
+def display_team():
+    """
+    Returns a list of all the teams ordered by their indexes, POST does not contain any arguments.
+    :rtype: object
+    """
+    pass
+
