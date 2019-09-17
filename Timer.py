63e36ab98129b317fce0e0ff27a7c99e669386ba
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-09-17 14:29:28 +0200

    Util (#9)
    * Created Util function.
    
    Added get_env method.
    added comments to all methods.
    
    * Created timer methods
    
    added timer methods.
    added comments to methods.
    
    * Added comments on some code.

--- Timer.py
+++ Timer.py
@@ -1,4 +1,5 @@
 from flask import Flask
+# Importing the different routes.
 from Routes.ProjectRoutes import pr
 from Routes.ManagerRoutes import mr
 from Routes.UserRoutes import ur
@@ -7,7 +8,25 @@ from Routes.TeamRoutes import tr
 
 app = Flask(__name__)
 
+# This Registers all the sub-routes.
 app.register_blueprint(pr)
 app.register_blueprint(tr)
 app.register_blueprint(mr)
 app.register_blueprint(ur)
+
+
+def verify_request():
+    """
+    Verifies the incoming request to make sure that the token that was passed is valid.
+    Returns true if the incoming request is valid false if it is not.
+    :rtype: bool
+    """
+    pass
+
+
+def error():
+    """
+    This will return a string response to the post request with a included error.
+    :rtype: str
+    """
+    pass
