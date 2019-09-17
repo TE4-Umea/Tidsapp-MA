a5d1dbcd8a6d07c0ac00dd12148f33927893a913
Aborren <elias.renman00@gmail.com>
2019-09-17 13:06:43 +0200

    Added ProjectRoutes routes
    Created routes.
    Added comments on expected behaviour.

--- Routes/ProjectRoutes.py
+++ Routes/ProjectRoutes.py
@@ -3,12 +3,43 @@ from flask import Blueprint, abort
 pr = Blueprint('pr', __name__)
 
 
-@pr.route('/create_project/<name>', methods=['POST'])
+@pr.route('/create_project/', methods=['POST'])
 def create_project(name):
     """
     Creates a new project for a user with the name of :param name.
-    Returns a response as a str message, redirects response from error if it fails..
+    Returns a response as a str message, redirects response from error if it fails.
     :param name: str
-    :rtype: Json Response
+    :rtype: str
     """
     pass
+
+
+@pr.route('/delete_project/', methods=['POST'])
+def delete_project():
+    """
+    Deletes a project from the database depending on the input written in the POST payload.
+    Returns a str saying Successfully deleted :placeholder_name: or error if error occurred or project did not exist.
+    :rtype: str
+    """
+    pass
+
+
+@pr.route('/update_project/', methods=['POST'])
+def update_project():
+    """
+    Updates a specific project name with a new name specified by the POST payload.
+    Returns a str saying Successfully updated project with a new name of :placeholder_name: or -
+    error if error occurred or project did not exist.
+    :rtype: str
+    """
+    pass
+
+
+@pr.route('/display_project/', methods=['POST'])
+def display_project():
+    """
+    Returns a list of all the projects ordered by their indexes POST does not contain any arguments.
+    :rtype: object
+    """
+    pass
+
