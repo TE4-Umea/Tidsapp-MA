ba4e20acf6ed3135aaa6134426236670bb25e36f
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-09-17 13:38:48 +0200

    Classes (#5)
    * Added ProjectRoutes routes
    
    Created routes.
    Added comments on expected behaviour.
    
    * updated comment.
    
    * Created TeamsRoutes routes.
    
    Added routes
    Wrote comments for the different methods.
    
    * created UserRoutes routes
    
    Added Routes methods.
    Added method comments.

--- Routes/UserRoutes.py
+++ Routes/UserRoutes.py
@@ -1,3 +1,46 @@
 from flask import Blueprint, abort
 
 ur = Blueprint('ur', __name__)
+
+
+@ur.route('/user_status/', methods=['POST'])
+def status():
+    """
+    Returns a response as a str message with the current users status.
+    This includes if the user is checked in, hours worked that day, hours worked that week..
+    Redirects response from error if it fails.
+    :rtype: str
+    """
+    pass
+
+
+@ur.route('/user_track/', methods=['POST'])
+def track():
+    """
+    Returns a response as a str message is either checked in or checked out.
+    Redirects response from error if it fails.
+    :rtype: str
+    """
+    pass
+
+
+@ur.route('/user_join_project/', methods=['POST'])
+def join_project():
+    """
+    This will make the POST payload specified User join a specified project that will be passed through as a argument -
+    in the payload.
+    Redirects response from error if it fails.
+    :rtype: str
+    """
+    pass
+
+
+@ur.route('/user_join_team/', methods=['POST'])
+def join_team():
+    """
+    This will make the POST payload specified User join a specified team that will be passed through as a argument in -
+    the payload.
+    Redirects response from error if it fails.
+    :rtype: str
+    """
+    pass
