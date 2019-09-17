4836b7d8dc64daac8bce18ae25d0ad4cfc59f997
Aborren <elias.renman00@gmail.com>
2019-09-17 13:10:27 +0200

    updated comment.

--- Routes/ProjectRoutes.py
+++ Routes/ProjectRoutes.py
@@ -6,9 +6,8 @@ pr = Blueprint('pr', __name__)
 @pr.route('/create_project/', methods=['POST'])
 def create_project(name):
     """
-    Creates a new project for a user with the name of :param name.
+    Creates a new project for a user with the name specified in POST payload.
     Returns a response as a str message, redirects response from error if it fails.
-    :param name: str
     :rtype: str
     """
     pass
