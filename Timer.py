3906f2c40cfda2616a93453af4406a0dced47892
Aborren <elias.renman00@gmail.com>
2019-09-17 14:26:53 +0200

    Created timer methods
    added timer methods.
    added comments to methods.

--- Timer.py
+++ Timer.py
@@ -11,3 +11,20 @@ app.register_blueprint(pr)
 app.register_blueprint(tr)
 app.register_blueprint(mr)
 app.register_blueprint(ur)
+
+
+def verify_request():
+    """
+    Verifies the incoming request to make sure that the token that was passed is valid.
+    Returns true if the incoming request is valid false if it is not.
+    :rtype: bool
+    """
+    pass
+
+
+def error():
+    """
+    This will return a string response to the post request with a included error.
+    :rtype: str
+    """
+    pass
