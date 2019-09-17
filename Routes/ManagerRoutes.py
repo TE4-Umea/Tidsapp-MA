72257ea367a5c0d9ac89d842a39f724885b92f09
august.moller <ag.man@live.se>
2019-09-17 13:49:05 +0200

    *started working on manager routes

--- Routes/ManagerRoutes.py
+++ Routes/ManagerRoutes.py
@@ -1,3 +1,20 @@
 from flask import Blueprint, abort
 
 mr = Blueprint('mr', __name__)
+
+@mr.route('/manager_status/', methods=['POST'])
+def manager_status():
+    """
+    show the status of all users
+    :return:
+    """
+    pass
+
+@mr.route('/manager_move/', methods=['POST'])
+def manager_move(name):
+    """
+    moves users to different teams
+    :param name:
+    :return:
+    """
+    pass
