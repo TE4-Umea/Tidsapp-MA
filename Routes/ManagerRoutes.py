5afd3036e0db745e71f4ea26c143e287d812196c
Agman10 <31540349+Agman10@users.noreply.github.com>
2019-09-17 14:20:34 +0200

    Manager routes (#8)
    * *started working on manager routes
    
    * *started working on manager routes

--- Routes/ManagerRoutes.py
+++ Routes/ManagerRoutes.py
@@ -1,3 +1,23 @@
 from flask import Blueprint, abort
 
 mr = Blueprint('mr', __name__)
+
+
+@mr.route('/manager_status/', methods=['POST'])
+def manager_status():
+    """
+    Show the status of all users specified with POST payload.
+    Returns a str of how long the user has been active.
+    :return: str
+    """
+    pass
+
+
+@mr.route('/manager_move/', methods=['POST'])
+def manager_move():
+    """
+    Moves users to a different team specified with POST payload
+    type the user name and then the name of the team.
+    :return: str
+    """
+    pass
