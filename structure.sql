106b476539a058978467b83329ae7352935b6c44
Elias Renman <33152039+Abborren@users.noreply.github.com>
2019-09-17 10:25:03 +0200

    set up repo (#1)
    Added gitignore.
    Added sql structure file.
    Added .env file.

---
+++ structure.sql
@@ -0,0 +1,35 @@
+CREATE TABLE `users` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255),
+  `checked_in` boolean,
+  `admin` boolean,
+  `current_team` int,
+  `current_project` int,
+  `created_at` datetime
+);
+
+CREATE TABLE `tracking` (
+  `id` int PRIMARY KEY,
+  `checked_in` boolean,
+  `user_id` int,
+  `project_id` int,
+  `timestamp` datetime
+);
+
+CREATE TABLE `project` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+CREATE TABLE `teams` (
+  `id` int PRIMARY KEY,
+  `name` varchar(255)
+);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`current_team`) REFERENCES `teams` (`id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`current_project`) REFERENCES `project` (`id`);
+
+ALTER TABLE `tracking` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `tracking` ADD FOREIGN KEY (`project_id`) REFERENCES `project` (`id`);
