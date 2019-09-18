1be7c56d27b5c83ae2ded8530fb1c36df03c82e5
Abborren <elias.renman00@gmail.com>
2019-09-18 17:29:23 +0200

    fixed timer

--- timer.py
+++ timer.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from dotenv import load_dotenv
 
-load_dotenv()
 # Importing the different routes.
 from Routes.project_routes import pr
 from Routes.manager_routes import mr
 from Routes.user_routes import ur
 from Routes.team_routes import tr
-
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -16,6 +15,8 @@ app.register_blueprint(pr)
 app.register_blueprint(tr)
 app.register_blueprint(mr)
 app.register_blueprint(ur)
+
+app.run()
 
 
 def verify_request():
