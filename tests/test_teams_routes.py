84d1371e4a6b9ba41cd4e8d96fd94811b75ca773
DavidPersson <david.persson@elev.ga.ntig.se>
2019-09-19 10:45:57 +0200

    "Changed comment fnutts"

--- tests/test_teams_routes.py
+++ tests/test_teams_routes.py
@@ -9,11 +9,11 @@ class TestCreateTeam(unittest.TestCase):
         self.app = app.test_client()
 
     def test_create_team(self):
-        '''
+        """
         This tries to create a team.
         The expected outcome is the return statement '200 OK'
         And the message 'Created team successfully'
-        '''
+        """
 
         # Python dictionary
         payload = {
@@ -32,12 +32,12 @@ class TestCreateTeam(unittest.TestCase):
         self.assertEqual(rv.data, "Created team successfully")
 
     def test_delete_non_existent_team(self):
-        '''
+        """
         This tries to delete a non existent team.
         The expected outcome is the return statement '200 OK'
         And an error message
         'Error has occurred, The specified team does not exist'
-        '''
+        """
 
         # Python dictionary
         payload = {
@@ -55,12 +55,12 @@ class TestCreateTeam(unittest.TestCase):
         self.assertEqual(rv.data, "Error has occurred, The specified team does not exist")
 
     def test_update_existing_team(self):
-        '''
+        """
         This tries to updating an existing team.
         The expected outcome is the return statement '200 OK'
         And if no teams exist the statement
         'The specified team does not exist'
-        '''
+        """
 
         # This parses dictionary to a json.
         json_obj = json.dumps({
@@ -82,12 +82,12 @@ class TestCreateTeam(unittest.TestCase):
         self.assertEqual(rv.data, "The specified team does not exist")
 
     def test_display_team(self):
-        '''
+        """
         This tries to create a team.
         Then prints a list of all teams.
         The expected outcome is the return statement '200 OK'
-        And the message 'Heres a list of all teams.\n  *test_Team'
-        '''
+        And the message 'Here's a list of all teams.\n  *test_Team'
+        """
 
         # Python dictionary
         payload = {
@@ -104,35 +104,4 @@ class TestCreateTeam(unittest.TestCase):
         # Makes sure that the response status is 200.
         assert rv.status == '200 OK'
         # Asserts that the response is the what is expected.
-        self.assertEqual(rv.data, "Heres a list of all teams.\n * test_Team")
-
-
-
-
-
-
-
-
-
-
-
-    # def test_delete_non_existent_project(self):
-    #     """
-    #     This tries to delete a non existent project.
-    #     Expected outcome is a return status of 200 and a error message saying.
-    #     `Error has occurred, The specified project does not exist`
-    #     """
-    #     # Python dictionary
-    #     payload = {
-    #         "token": "UzM1Hasfas231sdcUZrVbD76JpmP",
-    #     }
-    #     # This parses dictionary to a json.
-    #     json_obj = json.dumps(payload)
-    #
-    #     # Sends a POSt request to the endpoint "/delete_project/" with the payload of json_obj.
-    #     rv = self.app.post('/delete_project/', json=json_obj)
-    #
-    #     # Makes sure that the response status is 200.
-    #     assert rv.status == '200 OK'
-    #     # Asserts that the response is the what is expected.
-    #     self.assertEqual(rv.data, "Error has occurred, The specified project does not exist")
+        self.assertEqual(rv.data, "Here's a list of all teams.\n * test_Team")
