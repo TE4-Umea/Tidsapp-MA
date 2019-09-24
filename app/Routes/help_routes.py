c3eb4533a4a6207e12d45a8c40fff1ada219f3c7
DavidPersson <david.persson@elev.ga.ntig.se>
2019-09-24 09:45:28 +0200

    Adjustments to the switch case function and hopefully works now

--- app/Routes/help_routes.py
+++ app/Routes/help_routes.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from flask import Blueprint, abort, request
 
@@ -14,11 +15,21 @@ def status():
     :return: str
     """
 
-def switch(i):
-    switcher={
-            0:'test',
-            1:'test1',
-            2:'test3',
-            3:'test4,*+7'
+
+def switch_demo(argument):
+    """
+    This is a function similar to a switch case function
+    Its purpose is to be able to print all of the different strings
+
+    :return:
+    """
+    switcher = {
+        1: "status",
+        2: "Help",
+        3: "test",
+        4: "test2"
 
     }
+    #Prints the function
+    func = switcher.get(argument, "test")
+    print(func())
